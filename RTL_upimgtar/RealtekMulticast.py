379f6a67435478dcf7dceba3a899203b3ceda67c
Troll338cz <59578676+Troll338cz@users.noreply.github.com>
2026-06-21 17:06:29 +0200

    Remove IPv6 test, Comtrend does not know how to network in U-Boot...

--- RTL_upimgtar/RealtekMulticast.py
+++ RTL_upimgtar/RealtekMulticast.py
@@ -2,8 +2,6 @@
 import socket, struct, sys, time, zlib
 
 MCAST_IP		= "224.1.2.3"	# Default, can be anything in 224.0.0.0/4
-MCAST_IPv6		= "ff02::16:"	# Comtrend GRG-4362
-USE_V6			= True
 MCAST_PORT		= 1234			# Default, can be anything, ignored in U-Boot
 FRAME_LEN		= 1024			# MUTLICAST_FRAME_LENGTH
 INTERVAL		= 0.01			# Realtek tool has: 1 / 5 / 10 / 20 / 50 / 100 / 200 ms, Default is 10ms
@@ -29,7 +27,7 @@ def build_packet(datatype, seqnum, image_data, img_len, img_crc):
 	pkt += data				# image_data[1024]
 	return pkt
 
-def build_partition_hdr(name=b"kernel", addrstart=0, addrend=0, cover_flag=0):
+def build_partition_hdr(name=b"kernel", addrstart, addrend, cover_flag):
 	name20 = name.ljust(20, b"\x00")[:20]
 	return struct.pack(">20sIIB3x", name20, addrstart, addrend, cover_flag)
 
@@ -44,23 +42,15 @@ def main():
 	img_len = len(fw)
 	# "image size should not over 64MB, one packet size if 1024"
 	if(img_len > (51200 * 1024)):
-		print(f"File {sys.argv[1]} is larger then multicast protocol maximum {51200 * 1024} bytes, unsupported.")
+		print(f"File {sys.argv[1]} is larger then multicast protocol maximum of {(51200 * 1024)} bytes.")
 		sys.exit(1)
 
 	img_crc = zlib.crc32(fw) & 0xFFFFFFFF
 
-	if USE_V6:
-		sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-		sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, 1)
-		scope_id = socket.if_nametoindex(sys.argv[2])
-		dst = (MCAST_IPv6, MCAST_PORT, 0, scope_id)
-	else:
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-		sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, bytes(sys.argv[2]+"\0", "utf8"))
-		dst = (MCAST_IP, MCAST_PORT)
-
-
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, bytes(sys.argv[2]+"\0", "utf8"))
+	dst = (MCAST_IP, MCAST_PORT)
 	
 	# Force "partiton_info_array[i].type == PARTI_TYPE_IMG", it will try upimgtar and upvmimg
 	part_hdr = build_partition_hdr(b"kernel", 0, img_len, 0)
@@ -69,7 +59,7 @@ def main():
 	sock.sendto(pkt, dst)
 	time.sleep(INTERVAL)
 
-	# 2) Data packets
+	# 1024 data chunks
 	total_frames = (img_len + FRAME_LEN - 1) // FRAME_LEN
 	for i in range(total_frames):
 		seq = i + 1
@@ -81,7 +71,7 @@ def main():
 		if seq % 50 == 0 or seq == total_frames:
 			print(f"sent {seq}/{total_frames}")
 
-	# 3) Resend INFO packet once more to trigger "same packet" finish detection
+	# Finish transfer by sending header again
 	pkt = build_packet(MUP_DATATYPE_INFO, 0, info_data, img_len, img_crc)
 	sock.sendto(pkt, dst)
 
